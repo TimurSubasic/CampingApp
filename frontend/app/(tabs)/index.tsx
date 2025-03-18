bc982dc59a28a0b56ed90aa519476ef80bc9c7d0
Zana Kapetanovic <zanakapetanovic@Sanjins-MacBook-Pro.local>
2025-03-18 12:26:18 +0100

    added Search UI

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,60 +1,64 @@
 import React, { useLayoutEffect, useState } from 'react';
-import { Image, Text, TouchableOpacity, View, StyleSheet, Platform, StatusBar, ImageBackground } from 'react-native';
+import { Image, Text, TouchableOpacity, View, StyleSheet, Platform, StatusBar, ImageBackground, TextInput } from 'react-native';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome5';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { useNavigation } from 'expo-router';
-import {useHeaderHeight} from '@react-navigation/elements'
+import { useHeaderHeight } from '@react-navigation/elements'
 import RecommendButtons from '@/components/rbuttons'
 import Places from '@/components/places'
 import places from '@/data/places.json'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Page() {
   const navigation = useNavigation();
   const headerHeight = useHeaderHeight();
   const [selectedCategory, setSelectedCategory] = useState<string>('all');
- 
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerTransparent: true,
       headerTitle: '',
       headerLeft: () => (
         <View style={{ flexDirection: 'row', alignItems: 'center', marginLeft: 10 }}>
-          <TouchableOpacity onPress={() => {}}>
-          <Ionicons name="menu-outline" size={30} color="white" style={styles.menuIcon} />
+          <TouchableOpacity onPress={() => { }}>
+            <Ionicons name="menu-outline" size={30} color="white" style={styles.menuIcon} />
           </TouchableOpacity>
         </View>
       ),
       headerRight: () => (
-        <TouchableOpacity onPress={() => {}}>
-        
+        <TouchableOpacity onPress={() => { }}>
+
         </TouchableOpacity>
       ),
     });
   }, [navigation]);
 
   // Select places from 'All' for 'Hidden Gems'
-  const hiddenGems = places.slice(0, 1); 
+  const hiddenGems = places.slice(0, 1);
 
   // Filter places based on category selection
   const filteredPlaces =
     selectedCategory === 'all'
       ? places
       : selectedCategory === 'hidden_gems'
-      ? hiddenGems
-      : [];
+        ? hiddenGems
+        : [];
 
   return (
     <View style={[styles.container]}>
       {/* Background image for the top section */}
       <ImageBackground
-        source={require('@/assets/images/camper.jpeg')} 
-        style={[styles.backgroundImage, { paddingTop: headerHeight }]} 
+        source={require('@/assets/images/camper.jpeg')}
+        style={[styles.backgroundImage, { paddingTop: headerHeight }]}
       >
         <View style={styles.headerContainer}>
         </View>
       </ImageBackground>
-
       <View style={styles.contentContainer}>
+      <View style={styles.searchContainer}>
+        <Ionicons name="search" size={20} color="#888" style={styles.searchIcon} />
+        <TextInput placeholder = 'Search' clearButtonMode= 'always' style={styles.searchBar}/>
+      </View>
         <RecommendButtons onCategorySelect={setSelectedCategory} />
         <Places places={filteredPlaces} />
       </View>
@@ -79,8 +83,29 @@ const styles = StyleSheet.create({
     right: 0,
     alignItems: 'center',
     paddingHorizontal: 15,
-    paddingTop: Platform.OS === 'ios' ? 40 : 20, 
+    paddingTop: Platform.OS === 'ios' ? 40 : 20,
   },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 20,
+    paddingHorizontal: 15,
+    paddingVertical: 8,
+    marginHorizontal: 10,
+    marginTop: -10, 
+  },
+  searchIcon: {
+    marginRight: 8,
+  },
+  searchBar: {
+    flex: 1,
+    fontSize: 16,
+    color: '#333',
+  },
+  
   menuIcon: {
     padding: 10,
   },
@@ -92,5 +117,6 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 20,
     overflow: 'hidden',
     marginTop: -30,
+    paddingTop: 30,
   },
 });
--- frontend/components/places.tsx
+++ frontend/components/places.tsx
@@ -1,6 +1,6 @@
 import { FlatList, ListRenderItem, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
-import { placesTypes } from '@/types/placesTypes'; 
+import { placesTypes } from '@/types/placesTypes';
 
 
 type Props = {
@@ -32,7 +32,7 @@ const Places = ({ places }: Props) => {
         data={places}
         renderItem={renderItems}
         keyExtractor={(item) => item.id.toString()}
-        horizontal={true} 
+        horizontal={true}
         showsHorizontalScrollIndicator={false} // Hides the scroll indicator (optional)
       />
     </View>
@@ -40,62 +40,62 @@ const Places = ({ places }: Props) => {
 };
 
 const styles = StyleSheet.create({
-    listContainer: {
-      flex: 1,
-      padding: 10, 
-    },
-    box: {
-      backgroundColor: '#f8f8f8', 
-      borderRadius: 6, 
-      marginRight: 5,
-      overflow: 'hidden',
-      shadowColor: '#000', 
-      shadowOffset: { width: 0, height: 2 },
-      shadowOpacity: 0.15,
-      shadowRadius: 4,
-      elevation: 1, 
-      width: 200,
-      height: 150,
-    },
-    container: {
-      flexDirection: 'row',
-      padding: 6, // Smaller padding inside the box
-    },
-    // Commenting out image style for now
-    // image: {
-    //   width: 60, // Smaller image size
-    //   height: 60,
-    //   borderRadius: 6,
-    //   marginRight: 6,
-    // },
-    textContainer: {
-      flex: 1,
-      justifyContent: 'center',
-      flexDirection:'column',
-    },
-    name: {
-      fontSize: 15, 
-      fontWeight: 'bold',
-      marginBottom: 2, 
-    },
-    description: {
-        fontSize: 12,
-        color: '#555',
-        marginBottom: 2,
-        width: '90%',  
-        lineHeight: 18, 
-      },
-      
-    rating: {
-      fontSize: 13, 
-      color: '#888',
-    },
-    price: {
-      fontSize: 13, 
-      color: '#0D7377', 
-      marginTop: 2,
-      fontWeight: 'bold',
-    },
-  });
+  listContainer: {
+    flex: 1,
+    padding: 10,
+  },
+  box: {
+    backgroundColor: '#f8f8f8',
+    borderRadius: 6,
+    marginRight: 5,
+    overflow: 'hidden',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.15,
+    shadowRadius: 4,
+    elevation: 1,
+    width: 200,
+    height: 150,
+  },
+  container: {
+    flexDirection: 'row',
+    padding: 6, 
+  },
+  // Commenting out image style for now
+  // image: {
+  //   width: 60, // Smaller image size
+  //   height: 60,
+  //   borderRadius: 6,
+  //   marginRight: 6,
+  // },
+  textContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  name: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginBottom: 2,
+  },
+  description: {
+    fontSize: 12,
+    color: '#555',
+    marginBottom: 2,
+    width: '90%',
+    lineHeight: 18,
+  },
+
+  rating: {
+    fontSize: 13,
+    color: '#888',
+  },
+  price: {
+    fontSize: 13,
+    color: '#0D7377',
+    marginTop: 2,
+    fontWeight: 'bold',
+  },
+});
 
 export default Places;
